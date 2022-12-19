d50a0efe3693f8b4fc00c729082a7018bc38e4df
vpettersson <viktor.h.pettersson@gmail.com>
2022-12-19 16:05:37 +0100

    Removed counter

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -12,7 +12,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      
     </div>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
@@ -20,4 +20,4 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
