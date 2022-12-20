b0f769181f7e28d995653a6b1ec6ea82757ba907
vpettersson <viktor.h.pettersson@gmail.com>
2022-12-20 23:07:33 +0100

    Added Modalbox on info-btn click

--- vite-project/src/test.js
+++ vite-project/src/test.js
@@ -1,11 +1,47 @@
-let products = [];
+let candy = [];
 let containerEl = document.querySelector('.row');
+let shoppingcartCandy = [];
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+const modalTitle = document.querySelector(".titleCandy");
+const modalInfo = document.querySelector(".infoCandy");
+let candyImg = document.createElement("img");
+
+
+// close modal function
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+// close the modal when the close button and overlay is clicked
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+// close modal when the Esc key is pressed
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+});
+// open modal function
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
+// open modal event
+//openModalBtn.addEventListener("click", openModal); // denna gör ingenting längre då funktionen kallas på i onclick på knappen
+
+
 
 const getProducts = async () => {
     const data = await fetchProducts();
-    products = data.data;
+    products = data;
     renderProducts();
-    console.log(products.length);
+
 
 }
 
@@ -14,36 +50,62 @@ const fetchProducts = async () => {
     if (!res.ok) {
         throw new Error(`Could not fetch data, reason: ${res.status} ${res.statusText}`);
     }
-
     return await res.json();
+
 }
 
 const renderProducts = () => {
     console.log("rendering products...");
     //let products = res.json();
     let html = '';
-    products.forEach(product => {
+    //products.data.forEach(product => {
+    candy = products.data;
+    candy.forEach(product => {
+        //console.log(product);
 
-        console.log(product.images['thumbnail']);
+        //console.log(product.images['thumbnail']);
+
         let htmlSegment = `<div class="col-3"> 
             <img src="https://bortakvall.se${product.images['thumbnail']}"> 
             <p class="card-text">${product.name} ${product.price} kr</p>
-            <button type="button" class="btn btn-success">Add to cart</button> 
+            <button type="button" onclick="addToCart(${product.id})" class="btn btn-success">Add to cart</button> 
+            <button type="info" onclick="getInfo(${product.id})" class="btn btn-info">Info</button>
             </div>`;
 
         html += htmlSegment;
-    });
+        containerEl.innerHTML = html;
+    })
+};
 
-    containerEl.innerHTML = html;
+//containerEl.innerHTML = html;
 
+// funktion som tar in ID från klicket
+function addToCart(e) {
+    console.log(`you clicked product ${e}`);
+    const foundCandy = candy.find(candy => candy.id === e); // skapar nya variabel där man sätter variabeln till objectet som tillhör det specifika ID:et som klickades
+    console.log(foundCandy);
+    shoppingcartCandy.push(foundCandy); // pushar det klickade godiset till en tom array som vi sedan kommer lägga till kundkorgen
+    console.log('Shopping cart contains: ', shoppingcartCandy); // konsoll loggar "kundkorgen"
+}
 
-    //console.log(asd1);
+function getInfo(e) {
+    console.log(`you clicked info of product ${e}`);
+    const infoCandy = candy.find(candy => candy.id === e); // skapar ny variabel med objectet vars info-knapp klickas på.
+    //alert(infoCandy.description); // alert med description
+    openModal(); // öppnar modalboxen
+    modalInfo.innerHTML = (`${infoCandy.name} - ${infoCandy.price} kr`); //Placerar godisets namn & pris i modalen
+    modalInfo.innerHTML += (`${infoCandy.description}`); //Placerar godisets beskrivning i modalen
+    candyImg.src = (`https://bortakvall.se${infoCandy.images['large']}`); // hämtar den stora bilden från objectet för tillhörande godis
+    modalInfo.appendChild(candyImg); // renderar ut bilden i modalen
+    modalInfo.innerHTML += (`<button type="button" onclick="addToCart(${infoCandy.id})" class="btn btn-success">Add to cart</button>`); //lägger till samma knapp även i Modalen
 
 }
+
 
 fetchProducts();
 getProducts();
 renderProducts();
+
 
 
 
