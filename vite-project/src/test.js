367d56ab61361db3fb772b38a1610eae95c50f3d
christinen22 <christine.elin.louise@gmail.com>
2022-12-20 16:46:13 +0100

    Trying to store clicked items in array. :(

--- vite-project/src/test.js
+++ vite-project/src/test.js
@@ -1,5 +1,7 @@
 let products = [];
+let productCards = document.querySelector('.col-3');
 let containerEl = document.querySelector('.row');
+
 
 const getProducts = async () => {
     const data = await fetchProducts();
@@ -18,32 +20,67 @@ const fetchProducts = async () => {
     return await res.json();
 }
 
+
+
 const renderProducts = () => {
-    console.log("rendering products...");
+    //console.log("rendering products...");
     //let products = res.json();
     let html = '';
     products.forEach(product => {
 
-        console.log(product.images['thumbnail']);
-        let htmlSegment = `<div class="col-3"> 
-            <img src="https://bortakvall.se${product.images['thumbnail']}"> 
-            <p class="card-text">${product.name} ${product.price} kr</p>
-            <button type="button" class="btn btn-success">Add to cart</button> 
-            </div>`;
-
+        //console.log(product.images['thumbnail']);
+        let htmlSegment = `<div class="col-3">
+        <img src="https://bortakvall.se${product.images['thumbnail']}">
+        <p class="card-text">${product.name} ${product.price} kr</p>
+        <button type="button" onclick="storeItems(${product.id})" class="btn btn-success" id="addItem">Add to cart</button>
+        </div>`;
+    
         html += htmlSegment;
-    });
 
-    containerEl.innerHTML = html;
-
-
-    //console.log(asd1);
-
+    })
+      containerEl.innerHTML = html;
+   
 }
+
+function storeItems(productID) {
+    console.log(`You added ${productID} to your cart`);
+}
+
+const addItem = document.querySelector('#addItem');
+let itemsInCart = [];
+
+addItem.addEventListener('click', e => {
+    if(e.target.tagName === 'BUTTON') {
+        if (e.target.id === 'addItem') {
+            itemsInCart.push('test');
+        }
+    }
+  });
+
+  console.log(itemsInCart);
 
 fetchProducts();
 getProducts();
 renderProducts();
+
+
+
+
+/*let itemsInCart = [];
+
+addItem.addEventListener('click', e => {
+	e.preventDefault();
+	
+	if (e.target.tagName === 'BUTTON') {
+		
+    	if (e.target.id === 'addItem') {
+		console.log('hej');
+		
+    } 
+} }
+); */
+
+
 
 
 
